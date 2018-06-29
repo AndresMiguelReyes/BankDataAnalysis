d98dd0db587ecaac1a351e78778afffbe47fc050
Andres Reyes <areyes43@ucmerced.edu>
2018-06-29 10:05:27 -0700

    Update projectGLM.R

--- final project/projectGLM.R
+++ final project/projectGLM.R
@@ -24,11 +24,11 @@ if("X" %in% colnames(BankTrainPermanent))
   BankTrainPermanent = BankTrainPermanent[,-XLoc]
 }
 
-# XLoc = which(colnames(BankTwoThirdsTrain) == "duration")
-# BankTwoThirdsTrain = BankTwoThirdsTrain[,-XLoc]
-# 
-# XLoc = which(colnames(BankOneThirdTest) == "duration")
-# BankOneThirdTest = BankOneThirdTest[,-XLoc]
+XLoc = which(colnames(BankTwoThirdsTrain) == "duration")
+BankTwoThirdsTrain = BankTwoThirdsTrain[,-XLoc]
+ 
+XLoc = which(colnames(BankOneThirdTest) == "duration")
+BankOneThirdTest = BankOneThirdTest[,-XLoc]
 
 #### GLM AND LDA ####
 library(MASS)
@@ -47,27 +47,27 @@ print("misclass using glm: ")
 predict.glm2 = as.factor(predict.glm)
 predict.glm2 = ifelse(predict.glm2 == 1, "yes", "no")
 print( confusionMatrix(as.factor(predict.glm2), BankOneThirdTest$y) )
-#testTable = table(predict.glm, BankOneThirdTest$y)
-#glm.misclass = (testTable[1,1] + testTable[2,2])/sum(testTable)
-#print(testTable)
-#print(glm.misclass)
+testTable = table(predict.glm, BankOneThirdTest$y)
+glm.misclass = (testTable[1,1] + testTable[2,2])/sum(testTable)
+print(testTable)
+print(glm.misclass)
 print(summary(Bank.glm))
-#aUnder = auc(as.numeric(predict.glm),as.numeric(BankOneThirdTest$y) )
-#print(aUnder)
+aUnder = auc(as.numeric(predict.glm),as.numeric(BankOneThirdTest$y) )
+print(aUnder)
 
 
 Bank.lda = lda(y~.-cons.conf.idx-poutcome-previous-campaign-loan-housing-education
                -pdays-nr.employed,data=BankTwoThirdsTrain)
 lda.pred = predict(Bank.lda,newdata=BankOneThirdTest)
-#lda.table = table(lda.pred$class, BankOneThirdTest$y)
+lda.table = table(lda.pred$class, BankOneThirdTest$y)
 print("misclass using lda: ")
 print( confusionMatrix(lda.pred$class, BankOneThirdTest$y) )
-# lda.misclass = (lda.table[1,1] + lda.table[2,2])/sum(lda.table)
-# print(lda.table)
-# print(lda.misclass)
+lda.misclass = (lda.table[1,1] + lda.table[2,2])/sum(lda.table)
+print(lda.table)
+print(lda.misclass)
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-# aUnder = auc(as.numeric(lda.pred$class),as.numeric(BankOneThirdTest$y) )
-# print(aUnder)
+aUnder = auc(as.numeric(lda.pred$class),as.numeric(BankOneThirdTest$y) )
+print(aUnder)
 
 # New things that are important
 # default -> unknown
