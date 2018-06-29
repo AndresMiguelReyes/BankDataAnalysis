e7dc6794408b5cce9dc9cbb32a0589d65e99f812
Andres Reyes <areyes43@ucmerced.edu>
2018-06-29 10:07:39 -0700

    Update projectKNN.R

--- final project/projectKNN.R
+++ final project/projectKNN.R
@@ -17,15 +17,12 @@ test.X = cbind(BankOneThirdTest$euribor3m,BankOneThirdTest$month,BankOneThirdTes
 Y.Train =  BankTwoThirdsTrain$y
 knn.pred=knn(train.X,test.X,Y.Train,k=101)
 print( confusionMatrix(knn.pred, BankOneThirdTest$y) )
-#knn.table = table(knn.pred, BankOneThirdTest$y)
-# print("misclass using knn: ")
-# knn.misclass = (knn.table[1,1] + knn.table[2,2])/sum(knn.table)
-# print(knn.table)
-# print(knn.misclass)
-# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+knn.table = table(knn.pred, BankOneThirdTest$y)
+print("misclass using knn: ")
+knn.misclass = (knn.table[1,1] + knn.table[2,2])/sum(knn.table)
+print(knn.table)
+print(knn.misclass)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 aUnder = auc(as.numeric(knn.pred), as.numeric(BankOneThirdTest$y) )
 print(aUnder)
 plot(roc(as.numeric(knn.pred), as.numeric(BankOneThirdTest$y)), legacy.axes = TRUE, main= "KNN")
-# 
-# # find the best the k for knn using cv
-# shell.exec("https://www.youtube.com/watch?v=gAPnl-46iyQ")
